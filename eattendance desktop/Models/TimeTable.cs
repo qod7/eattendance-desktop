2180a16b9d0eda50637502f1d2209554a3aa2ca6
Aayush Subedi <subedi.aayush@gmail.com>
2016-04-11 21:15:22 +0545

    Added shift and timetable models

---
+++ eattendance desktop/Models/TimeTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eattendance_desktop.Models
+{
+    class TimeTable
+    {
+        public TimeSpan from;
+        public TimeSpan to;
+
+        public int offsetFrom;
+        public int offsetTo;
+
+        public int offsetUnit { get; set;  }
+
+        public TimeTable(TimeSpan from, TimeSpan to)
+        {
+            this.from = from;
+            this.to = to;
+
+            this.offsetFrom = 0;
+            this.offsetTo = 0;
+            this.offsetUnit = -1;
+        }
+
+        public int OffsetFrom
+        {
+            get { return offsetFrom; }
+            set { 
+                offsetFrom = value;
+
+                if (offsetUnit == -1) offsetUnit = OFFSET_MINUTES;
+            }
+        }
+
+        public int OffsetTo
+        {
+            get { return offsetTo; }
+            set
+            {
+                offsetTo = value;
+
+                if (offsetUnit == -1) offsetUnit = OFFSET_MINUTES;
+            }
+        }
+        
+        public static const int OFFSET_MINUTES = 1,
+            OFFSET_HOURS = 2;
+
+    }
+}
